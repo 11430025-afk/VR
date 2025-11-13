66353451728f9d355745241500a977d67b9c8eff
11430025-afk <11430025@sssh.tp.edu.tw>
2025-11-13 11:55:17 +0800

    11/13

---
+++ app-files/app-files/data.js
@@ -0,0 +1,239 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-street-view-360jpg-6",
+      "name": "Street View 360.jpg-6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3271873016245657,
+          "pitch": 0.005715827767758697,
+          "rotation": 0,
+          "target": "1-street-view-360jpg-7"
+        },
+        {
+          "yaw": -2.0574158765614907,
+          "pitch": -0.09462952823400883,
+          "rotation": 0,
+          "target": "3-street-view-360jpg-9"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-street-view-360jpg-7",
+      "name": "Street View 360.jpg-7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2553636630417166,
+          "pitch": 0.0745790012207852,
+          "rotation": 0,
+          "target": "0-street-view-360jpg-6"
+        },
+        {
+          "yaw": -1.9554184326310011,
+          "pitch": 0.07574867478516012,
+          "rotation": 0,
+          "target": "3-street-view-360jpg-9"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-street-view-360jpg-8",
+      "name": "Street View 360.jpg-8",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9547479694138161,
+          "pitch": 0.3108585926764764,
+          "rotation": 0,
+          "target": "3-street-view-360jpg-9"
+        },
+        {
+          "yaw": -2.8589804880808902,
+          "pitch": 0.11100122856110062,
+          "rotation": 0,
+          "target": "4-street-view-360jpg-10"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-street-view-360jpg-9",
+      "name": "Street View 360.jpg-9",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.660957767634434,
+          "pitch": -0.016539137416099692,
+          "rotation": 0,
+          "target": "1-street-view-360jpg-7"
+        },
+        {
+          "yaw": 1.2034551291251852,
+          "pitch": 0.15889997635302144,
+          "rotation": 0,
+          "target": "2-street-view-360jpg-8"
+        },
+        {
+          "yaw": -2.961937458845222,
+          "pitch": -0.0010981801653748846,
+          "rotation": 0,
+          "target": "0-street-view-360jpg-6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-street-view-360jpg-10",
+      "name": "Street View 360.jpg-10",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2264823677018004,
+          "pitch": 0.09837798105703754,
+          "rotation": 0,
+          "target": "2-street-view-360jpg-8"
+        },
+        {
+          "yaw": 0.5307639061016225,
+          "pitch": 0.08773474401510484,
+          "rotation": 0,
+          "target": "0-street-view-360jpg-6"
+        },
+        {
+          "yaw": 0.9693442915635142,
+          "pitch": 0.2563827956693103,
+          "rotation": 0,
+          "target": "1-street-view-360jpg-7"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "11/13",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
